5c1323bf1d7c1c1514f4f4429381457f2ff0ffba
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-06 23:44:13 +0200

    add Tailwindcss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <main>
+    <main className="bg-amber-600">
       <div>
         <h1>Welcome to My Blog</h1>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   plugins: [
     devtools(),
     netlify(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
