51d6bfe80ac7c66f81556c1ace8ecdee74d5dc3e
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-07 00:37:26 +0200

    Build Navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,13 +2,13 @@ import { Link } from "@tanstack/react-router";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
-        <Link to="/">
+    <header className="border-b ">
+      <nav className="max-container flex items-center justify-between py-4">
+        <Link to="/" className="font-bold text-2xl">
           Dev<span className="text-primary">Blog</span>
         </Link>
         <ul>
-          <li>
+          <li className="text-accent-foreground hover:text-primary">
             <Link to="/">Blog</Link>
           </li>
         </ul>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -47,7 +47,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="light" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <HeadContent />
