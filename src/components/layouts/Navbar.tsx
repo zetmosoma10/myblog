f9777955a35f72a94ce225932efd7c80d371783b
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-07 09:46:12 +0200

    style Nav links

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,5 +1,16 @@
 import { Link } from "@tanstack/react-router";
 
+const links = [
+  {
+    label: "Articles",
+    to: "/",
+  },
+  {
+    label: "About",
+    to: "/",
+  },
+];
+
 const Navbar = () => {
   return (
     <header className="border-b ">
@@ -7,10 +18,22 @@ const Navbar = () => {
         <Link to="/" className="font-bold text-2xl">
           Dev<span className="text-primary">Blog</span>
         </Link>
-        <ul>
-          <li className="text-accent-foreground hover:text-primary">
-            <Link to="/">Blog</Link>
-          </li>
+        <ul className="flex gap-6">
+          {links.map((link) => (
+            <li
+              key={link.to}
+              className="text-accent-foreground hover:text-primary"
+            >
+              <Link
+                activeProps={{
+                  className: "font-semibold  text-primary",
+                }}
+                to={link.to}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
