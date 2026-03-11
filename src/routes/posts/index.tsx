4d76dc8383b839c690d803c613a41154fe1bfb90
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-11 21:45:15 +0200

    add Search Input

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -1,6 +1,12 @@
 import PostCard from "#/components/PostCard";
 import useGetPosts, { postsQueryOptions } from "#/hooks/useGetPosts";
 import { createFileRoute } from "@tanstack/react-router";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "#/components/ui/input-group";
+import { SearchIcon } from "lucide-react";
 
 export const Route = createFileRoute("/posts/")({
   component: RouteComponent,
@@ -18,9 +24,15 @@ function RouteComponent() {
         <h2 className="text-3xl md:text-4xl font-extrabold mb-2 text-foreground">
           Posts
         </h2>
-        <p className="text-muted-foreground mb-8">
+        <p className="text-muted-foreground">
           Explore the latest thoughts on technology, design and programming.
         </p>
+        <InputGroup className="mt-6 py-5 mb-8 w-full md:w-[35%]">
+          <InputGroupInput placeholder="Search..." />
+          <InputGroupAddon align="inline-start">
+            <SearchIcon className="text-muted-foreground" />
+          </InputGroupAddon>
+        </InputGroup>
         <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-7">
           {posts?.map((post) => (
             <PostCard
