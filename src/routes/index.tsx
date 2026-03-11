501dedacd1a5130c2625820b0c3ff60499a639e1
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-11 23:45:27 +0200

    add features section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,32 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Hero from "#/components/Hero";
+import { getPosts } from "#/server/postsSeverFunctions";
+import PostCard from "#/components/PostCard";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+  component: App,
+  loader: async () => {
+    return await getPosts();
+  },
+  staleTime: 1000 * 60 * 10,
+});
 
 function App() {
+  const posts = Route.useLoaderData();
+  const features = posts.slice(0, 3);
+
   return (
     <main className="bg-background">
       <Hero />
+
+      <div>
+        <h2 className="text-foreground text-3xl md:text-4xl mb-7">Features</h2>
+        <section className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-7">
+          {features.map((post) => (
+            <PostCard key={post._id} {...post} />
+          ))}
+        </section>
+      </div>
     </main>
   );
 }
