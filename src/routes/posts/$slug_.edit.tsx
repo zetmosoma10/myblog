caec1a6cf25070476c0c260408bbe7ba7ac0d810
Zet Mosoma <zetmosoma10@gmail.com>
2026-03-13 17:52:32 +0200

    Render a prepopulated post form component

--- src/routes/posts/$slug_.edit.tsx
+++ src/routes/posts/$slug_.edit.tsx
@@ -1,9 +1,36 @@
+import BackLink from "#/components/BackLink";
+import PostForm from "#/components/PostForm";
+import { postQueryOptions } from "#/hooks/useGetPost";
+import type { PostType } from "#/types/post.type";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/posts/$slug_/edit")({
+  ssr: false,
   component: RouteComponent,
+  loader: async ({ context, params }) => {
+    const { slug } = params;
+    const { queryClient } = context;
+
+    await queryClient.ensureQueryData(postQueryOptions(slug));
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/posts/$slug/edit"!</div>;
+  const { slug } = Route.useParams();
+  const { queryClient } = Route.useRouteContext();
+  const post = queryClient.getQueryData<PostType>(["post", slug]);
+
+  return (
+    <section className="max-container">
+      <div className="py-12">
+        <BackLink />
+        <div>
+          <h2 className="text-4xl font-bold text-foreground leading-tight mb-6">
+            Edit Post
+          </h2>
+          <PostForm type="Edit" post={post} />
+        </div>
+      </div>
+    </section>
+  );
 }
