6a06f36c9e8079b661950cf3018bafc8397c07b9
Zet Mosoma <zetmosoma10@gmail.com>
2026-04-02 09:46:23 +0200

    add base prop to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
+  base: "/",
   plugins: [
     devtools(),
     netlify(),
