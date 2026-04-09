b18588b946f7b3bc3cb27edce7523cb210971f76
Zet Mosoma <zetmosoma10@gmail.com>
2026-04-09 12:59:57 +0200

    feat: disable link crawling in tanstackStart configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ const config = defineConfig({
     tanstackStart({
       prerender: {
         enabled: true,
-        crawlLinks: true,
+        crawlLinks: false,
       },
       pages: [{ path: "/" }, { path: "/about" }],
     }),
